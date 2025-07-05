96a193334680b0dc0d49c3f09ff934a4f2d89a7a
gneval9 <142158863+gneval9@users.noreply.github.com>
2025-07-05 12:47:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='framedirect',
-    version='0.5.0',
+    version='0.5.1',
     author='gneval9 Software',
     author_email='gneval99@gmail.com',
     description='Una librería para dibujar píxeles usando el framebuffer de Linux en Python.',
